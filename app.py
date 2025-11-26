93a75aba01928bf6d09be60633c7505a6daaec00
Yash <rajyashkarn@gmail.com>
2025-11-26 10:52:56 +0530

    models reads image as numpy arrays

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi.responses import  HTMLResponse,Response,StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from ultralytics import YOLO
 import cv2
-import numpy
+import numpy as np
 import asyncio
 
 app = FastAPI()
@@ -20,3 +20,22 @@ accident_model.overrides["classes"] =[0]
 
 model = [fire_model,smoke_model,helmet_model,accident_model]
 
+app.mount("/static",StaticFiles(directory="static"),name = "static")
+
+
+@app.get("/",response_class=HTMLResponse)
+def home_page():
+    with open("static/index.html") as f:
+         return HTMLResponse(f.read())
+
+@app.post("/predict-image")
+async def predict_image(file:UploadFile=File(...)):
+
+
+    try:
+        image_bytes = await file.read()
+        np_img = np.frombuffer(image_bytes, np.uint8)
+        img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+
+
+        
