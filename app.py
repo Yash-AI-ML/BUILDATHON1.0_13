5315838589e9e9be8b6d82cbbf7f1a66f875ad6a
Yash <rajyashkarn@gmail.com>
2025-11-26 10:16:18 +0530

    imported modules

--- app.py
+++ app.py
@@ -5,3 +5,18 @@ from ultralytics import YOLO
 import cv2
 import numpy
 import asyncio
+
+app = FastAPI()
+
+fire_model = YOLO(),
+smoke_model= YOLO(),
+helmet_model= YOLO(),
+accident_model= YOLO(),
+
+fire_model.overrides["classes"] =[0]
+smoke_model.overrides["classes"] =[0]
+helmet_model.overrides["classes"] =[0,1]
+accident_model.overrides["classes"] =[0]
+
+model = [fire_model,smoke_model,helmet_model,accident_model]
+
